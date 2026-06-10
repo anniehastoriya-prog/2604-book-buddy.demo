bc9b98d57765c80c304340a602ba60fee817f4d0
Annie <anniehastoriya@gmail.com>
2026-06-10 15:21:57 -0500

    recreating files

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,29 @@
-import { Routes, Route } from "react-router-dom";
-import NavBar from "./components/NavBar";
-import BookList from "./components/BookList";
+import { Routes, Route } from "react-router";
+import Layout from "./layout/Layout";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import BooksPage from "./books/BooksPage";
+import BookDetails from "./books/BookDetails";
+import AccountPage from "./account/AccountPage";
+import Error404 from "./Error404.jsx";
 
+/**
+ * Book Buddy is a platform where users can browse a catalog of books.
+ * Anyone can browse the site and make an account, and users with an
+ * account will be able to reserve and return books.
+ */
 export default function App() {
   return (
-    <>
-      <NavBar />
-      <main>
-        <Routes>
-          <Route path="/" element={<BookList />} />
-          <Route path="/books" element={<BookList />} />
-          <Route path="/books/:id" element={<p>Book details coming soon.</p>} />
-          <Route path="/account" element={<p>Account page coming soon.</p>} />
-          <Route path="/register" element={<p>Register page coming soon.</p>} />
-          <Route path="/login" element={<p>Login page coming soon.</p>} />
-        </Routes>
-      </main>
-    </>
+    <Routes>
+      <Route element={<Layout />}>
+        <Route index element={<BooksPage />} />
+        <Route path="/books" element={<BooksPage />} />
+        <Route path="/books/:id" element={<BookDetails />} />
+        <Route path="/account" element={<AccountPage />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="*" element={<Error404 />} />
+      </Route>
+    </Routes>
   );
 }
