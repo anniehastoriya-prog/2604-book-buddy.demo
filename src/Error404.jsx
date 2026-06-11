3fa2d27583df6cfbe2522eb8cefb2ca835bf7c5e
Annie <anniehastoriya@gmail.com>
2026-06-10 23:15:34 -0500

    accountpage created

---
+++ src/Error404.jsx
@@ -0,0 +1,9 @@
+/** Displayed when no route matches the requested URL */
+export default function Error404() {
+  return (
+    <>
+      <h1>404</h1>
+      <p>Sorry, we couldn't find that page!</p>
+    </>
+  );
+}
