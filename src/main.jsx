519bf42429042ac1d089e13badc68beb1bf937fa
Annie <anniehastoriya@gmail.com>
2026-06-13 16:37:38 -0500

    book folder create with written code the files booklist, bookdetails, and bookpage

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router";
 import App from "./App";
-import { AuthProvider } from "./context/AuthContext";
+import { AuthProvider } from "./auth/AuthContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
